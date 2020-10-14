52266e51902a88fee2cd2fd601ebe31bcc64b4c7
Andrhé Victor <andrhevictor@gmail.com>
2020-10-14 12:46:32 -0400

    Handling exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
+import "express-async-errors";
 
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 import "./database/connection";
 
@@ -10,5 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333);
